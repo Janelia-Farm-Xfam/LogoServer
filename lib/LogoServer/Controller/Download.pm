33f1f3dc9c0c7e142772361e1660eb1ce854e145
Jody Clements <clementsj@janelia.hhmi.org>
2013-06-25 10:37:04 -0400

    Adds option to download generated hmm file
    There is now a link that downloads the hmm file generated from multiple
    sequence alignments. This is the file used to create the logo.

--- lib/LogoServer/Controller/Download.pm
+++ lib/LogoServer/Controller/Download.pm
@@ -21,8 +21,8 @@ Catalyst Controller.
 
 =cut
 
-sub index :Path :Args(1) {
-  my ( $self, $c, $uuid ) = @_;
+sub index :Path :Args(2) {
+  my ( $self, $c, $uuid, $type ) = @_;
   my @dirs = split /-/, $uuid;
   # mkdir the path
   my $data_dir = $c->config->{logo_dir} .'/'. join '/', @dirs;
@@ -34,25 +34,50 @@ sub index :Path :Args(1) {
   }
 
   open my $hmm, '<', $hmm_path;
-  my $alphabet = 'dna';
 
-  # check to see if HMM is DNA or AA
-  while (my $line = <$hmm>) {
-    if ($line =~ /^ALPH/) {
-      if ($line =~ /amino/) {
-        $alphabet = 'aa';
+  if ($type eq 'image') {
+
+    my $alphabet = 'dna';
+    # check to see if HMM is DNA or AA
+    while (my $line = <$hmm>) {
+      if ($line =~ /^ALPH/) {
+        if ($line =~ /amino/) {
+          $alphabet = 'aa';
+        }
+        last;
       }
-      last;
     }
+
+
+    # run the logo generation
+    my $png = $c->model('LogoGen')->generate_png($hmm_path,$alphabet);
+    $c->response->content_type('image/png');
+    $c->response->body($png);
+  }
+  else {
+    my $hmm_text = '';
+    while (my $line = <$hmm>) {
+      $hmm_text .= $line;
+    }
+    $c->response->content_type('text/plain');
+    $c->response->body($hmm_text);
   }
 
   close $hmm;
+  return;
+}
 
-  # run the logo generation
-  my $png = $c->model('LogoGen')->generate_png($hmm_path,$alphabet);
-  $c->response->content_type('image/png');
-  $c->response->body($png);
+sub missing_uuid : Path : Args(0) {
+  my ( $self, $c ) = @_;
+  $c->res->redirect($c->uri_for('/'));
+  return;
+}
 
+sub missing_type : Path : Args(1) {
+  my ($self, $c, $uuid) = @_;
+  # by default we redirect to the image if no type is given.
+  my $uri = $c->uri_for($uuid, 'image');
+  $c->res->redirect($uri);
   return;
 }
 

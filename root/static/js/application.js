46e4ab8f871b3a9c6d14a52522843cd6a2d5c4f9
Jody Clements <clementsj@janelia.hhmi.org>
2013-06-27 16:31:36 -0400

    Lots of style changes.

--- root/static/js/application.js
+++ root/static/js/application.js
@@ -1,3 +1,4 @@
+/*global alphabet, logo_data */
 (function ($) {
   "use strict";
 
@@ -8,6 +9,12 @@
     if (typeof logo_data !== 'undefined') {
       hmm_logo = $('#logo_graphic').hmm_logo({height_toggle: 1, column_width: 34, data: logo_data, alphabet: alphabet});
     }
+
+    $('#try_again').on('click', function (e) {
+      e.preventDefault();
+      $(this).siblings('div').toggle('fast');
+    });
+
   });
 
 })(jQuery);

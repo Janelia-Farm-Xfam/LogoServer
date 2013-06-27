8be283f84b18c5d713633c581179389fd181b847
Jody Clements <clementsj@janelia.hhmi.org>
2013-06-27 17:10:59 -0400

    Search again box is on by default again.

--- root/static/js/application.js
+++ root/static/js/application.js
@@ -9,12 +9,6 @@
     if (typeof logo_data !== 'undefined') {
       hmm_logo = $('#logo_graphic').hmm_logo({height_toggle: 1, column_width: 34, data: logo_data, alphabet: alphabet});
     }
-
-    $('#try_again').on('click', function (e) {
-      e.preventDefault();
-      $(this).siblings('div').toggle('fast');
-    });
-
   });
 
 })(jQuery);

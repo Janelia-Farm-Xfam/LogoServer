42faaf8d178bca2f07d12b0f54b1f631f3022c69
Jody Clements <clementsj@janelia.hhmi.org>
2013-07-02 09:37:53 -0400

    Allow people to restart the tour.
    If they cancel it or just want to see it again, this will allow them to
    restart from the beginning.

--- root/static/js/application.js
+++ root/static/js/application.js
@@ -7,7 +7,24 @@
 
   $(document).ready(function () {
     if (typeof logo_data !== 'undefined') {
-      hmm_logo = $('#logo_graphic').hmm_logo({height_toggle: 1, column_width: 34, data: logo_data, alphabet: alphabet, scaled_max:true});
+      hmm_logo = $('#logo_graphic').hmm_logo({height_toggle: 1, column_width: 34, data: logo_data, alphabet: alphabet, scaled_max: true});
+    }
+
+    if ($("#joyRideTipContent").length > 0) {
+      $("#joyRideTipContent").joyride({
+        'autoStart' : true,
+        'postRideCallback' : function () {
+          $('.tour_control').show('slow');
+        },
+        'preRideCallback' : function () {
+          $('.tour_control').hide();
+        }
+      });
+
+      $('#restart').on('click', function () {
+        console.log('restarting');
+        $("#joyRideTipContent").joyride('restart');
+      });
     }
   });
 

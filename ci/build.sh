9c6ae0fde7775fa8a7f9de9beda9abd03963a3d9
neomorphic <jody@neomorphic.co.uk>
2013-09-06 15:45:41 -0400

    Need to remove existing minified js.
    Since we are using git to do the checkouts for this project, we dont
    blow away and checkout a whole new directory like we did for subversion.
    This is faster, but does leave behind build artifacts. If we dont remove
    those, then they can get rolled into the next build and cause
    duplications of javascript in the minified files.

--- ci/build.sh
+++ ci/build.sh
@@ -8,9 +8,14 @@ export PATH=/opt/bin:$PATH
 
 /opt/bin/lessc --yui-compress root/static/css/main.less root/static/css/main.min.css
 
-# minify and compress the javascript
+# clear out old build files
+rm /opt/www/Skylign/current/root/static/js/main.js
+rm /opt/www/Skylign/current/root/static/js/main.min.js
 
+
+# minify and compress the javascript
 cat /opt/www/Skylign/current/root/static/js/*.js > /opt/www/Skylign/current/root/static/js/main.js
+
 
 /usr/bin/java -jar /opt/lib/java/compiler.jar --js /opt/www/Skylign/current/root/static/js/main.js --js_output_file /opt/www/Skylign/current/root/static/js/main.min.js
 

52071ddf94faae9ae7b865440f22eff9fc56f71f
neomorphic <jody@neomorphic.co.uk>
2013-09-09 17:30:33 -0400

    Add 404 page.
    Much nicer than the "page not found" text

--- lib/LogoServer/Controller/Root.pm
+++ lib/LogoServer/Controller/Root.pm
@@ -239,9 +239,8 @@ Standard 404 error page
 =cut
 
 sub default :Path {
-    my ( $self, $c ) = @_;
-    $c->response->body( 'Page not found' );
-    $c->response->status(404);
+  my ( $self, $c ) = @_;
+  $c->response->status(404);
 }
 
 =head2 end
